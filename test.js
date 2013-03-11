c75858a2b1d684d0587f03c161ff588dd1bcf2e8
Corey Butler <corey@coreybutler.com>
2013-03-11 16:06:50 -0500

    Unnecessary

--- test.js
+++
@@ -1,9 +0,0 @@
-var x = require('./lib/node-windows');
-
-var pth = "C:\\Users\\Corey\\AppData\\Roaming\\npm\\node_modules\\ngn-mechanic\\lib\\mechanic\\app.js";
-console.log(pth);
-//x.service.start('NGN Mechanic',pth);
-//x.service.remove('NGN Mechanic');
-x.list(function(list){
-  console.log(list);
-},true);
